a894872aacdae3ff4b761071d8165ba8b067ad38
Duc Thang <tranducthang1505@gmail.com>
2021-01-02 16:27:47 +0700

    Create SingletonHolder.kts

---
+++ SingletonHolder.kts
@@ -0,0 +1,89 @@
+package com.kotlin.utils.singleton
+
+/**
+ * What if you need pass some arguments for initialization jus like parameterized constructor?
+ * Since we can't use constructor with the object keyword. So, we need to find some other way of
+ * doing the same.
+ *
+ * We can archive this by using a SingletonHolder class. 
+ * Also, to make it thread-safe, we need to have a way of synchronized a double check locking principle.
+ */
+open class SingletonHolder<out T : Any, in A>(creator: (A) -> T) {
+    private var creator: ((A) -> T)? = creator
+
+    @Volatile
+    private var instance: T? = null
+
+    protected fun getInstanceInternal(arg: A): T {
+        val checkInstance = instance
+        if (checkInstance != null) return checkInstance
+        return synchronized(this) {
+            val checkInstanceAgain = instance
+            if (checkInstanceAgain != null) checkInstanceAgain
+            else {
+                val created = creator!!(arg)
+                instance = created
+                creator = null
+                created
+            }
+        }
+    }
+}
+
+/**
+ * If you need to pass only ONE argument to the constructor of the singleton class.
+ * Make companion object extended from [SingletonHolderSingleArg] for best match.
+ *
+ * Ex:
+    class AppRepository private constructor(private val db: Database) {
+        companion object : SingletonHolderSingleArg<ArgKotlinSingleton, Database>(::AppRepository)
+    }
+ 
+ * Uses:
+    val appRepository =  AppRepository.getInstance(db)
+ */
+open class SingletonHolderSingleArg<out T : Any, in A>(creator: (A) -> T) :
+    SingletonHolder<T, A>(creator) {
+    fun getInstance(arg: A): T = getInstanceInternal(arg)
+}
+
+/**
+ * If you need to pass TWO arguments to the constructor of the singleton class.
+ * Extended from [SingletonHolderPairArgs] for best match.
+ *
+ * Ex:
+    class AppRepository private constructor(private val db: Database, private val apiService: ApiService) {
+        companion object : SingletonHolderPairArgs<ArgKotlinSingleton, Database, ApiService>(::AppRepository)
+    }
+ *
+ * Uses:
+    val appRepository =  AppRepository.getInstance(db, apiService)
+ */
+open class SingletonHolderPairArgs<out T : Any, in A, in B>(creator: (A, B) -> T) :
+    SingletonHolder<T, Pair<A, B>>(creator = { (a, b) -> creator(a, b) }) {
+
+    fun getInstance(arg1: A, arg2: B) = getInstanceInternal(Pair(arg1, arg2))
+}
+
+
+/**
+ * If you need to pass THREE arguments to the constructor of the singleton class. 
+ * Extended from [SingletonHolderTripleArgs] for the best match.
+ *
+ * Ex:
+    class AppRepository private constructor(
+       private val db: Database,
+       private val apiService: ApiService,
+       private val storage : Storage
+    ) {
+       companion object : SingletonHolderTripleArgs<ArgKotlinSingleton, Database, ApiService, Storage>(::AppRepository)
+    }
+ *
+ * Uses:
+    val appRepository =  AppRepository.getInstance(db, apiService, storage)
+ */
+open class SingletonHolderTripleArgs<out T : Any, in A, in B, in C>(creator: (A, B, C) -> T) :
+    SingletonHolder<T, Triple<A, B, C>>(creator = { (a, b, c) -> creator(a, b, c) }) {
+
+    fun getInstance(arg1: A, arg2: B, arg3: C) = getInstanceInternal(Triple(arg1, arg2, arg3))
+}
